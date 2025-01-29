79d24a794880d3dd4e5acd79527eebf9c3f96713
Alex Batista da Costa <alex.batista@dnacenter>
2025-01-28 21:59:21 -0300

    Deploy para GitHub Pages

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -3,6 +3,11 @@
 * Copyright 2013-2025 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
 */
+/*!
+* Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
+* Copyright 2013-2025 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+*/
 //
 // Scripts
 // 
